037587174d347adeb6472f5b0077f290b481fc5f
Carlos Sierra <csierra.developer@gmail.com>
2025-06-26 09:51:20 -0700

    Create basic REST controller

---
+++ src/main/java/com/csierra/demo03072025/Demo03072025Application.java
@@ -0,0 +1,13 @@
+package com.csierra.demo03072025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo03072025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo03072025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/csierra/demo03072025/Demo03072025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csierra.demo03072025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo03072025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
